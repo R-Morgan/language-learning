bc5f8627240e09a9abf28e58d363f7ddd3838521
R-Morgan <rmorgan@trystero.is>
2014-10-02 16:55:19 -0700

    Add simple two value summation prog

---
+++ addInts.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+  int num1;
+  int num2;
+  int sum;
+  cout<<"Enter an integer: ";
+  cin>> num1;
+  cin.ignore();
+  cout<< "Enter a second integer: ";
+  cin>> num2;
+  cin.ignore();
+  sum = num1 + num2;
+  cout<<"The sum of the numbers is: " <<sum <<"\n";
+  cin.get();
+}
+
