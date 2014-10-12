4a82e10ed5b1ea4931b42e7bfbb4f33d1f41581c
R-Morgan <rmorgan@trystero.is>
2014-10-12 10:37:06 -0700

    Add prog to explore strlen() function

---
+++ cmelearnc/ex-6/strlengtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(){ 
+    char *strex = "india";
+    int leng = strlen(strex);
+    printf("%d\n", leng);
+    
+    return 0;
+}
