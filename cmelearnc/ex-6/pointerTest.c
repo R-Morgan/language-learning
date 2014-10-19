7aeef894f057106353ea635487d0e5f0fb65e27d
R-Morgan <rmorgan@trystero.is>
2014-10-19 13:28:41 -0700

    Clean up spaces

--- cmelearnc/ex-6/pointerTest.c
+++ cmelearnc/ex-6/pointerTest.c
@@ -24,18 +24,26 @@ void pointerTest1(int agel[], char *namel[])
 void pointerTest2(int agel[], char *namel[])
 { 
     // initialise count and indexing variable
-    int count = sizeof(agel) / sizeof(int);
+    //  int count = sizeof(agel) / sizeof(int);
     int i = 0;
+    int size = sizeof(agel);
+
+    // initialise pointers
 
     int *cur_age = agel;
     char **cur_name = namel;
 
+    // get size of what is in pointer
+    int count = sizeof(*cur_age);
+    
     //index arrays method
-    for(i = 0; i < 5; i++){ 
-        printf("%s has %d years alive.\n", cur_name[i], cur_age[i]);
-        printf("count is %d\n", i);
+    for(i = 0; i < count; i++){ 
+        printf("%s has %d years alive.\n\n", cur_name[i], cur_age[i]);
+        printf("i is %d\n", i);
+        printf("count is %d\n", count);
     }
 
+    printf("size of agel is: %d\n", size);
     printf("---\n");
 }
 
@@ -43,9 +51,9 @@ void pointerTest2(int agel[], char *namel[])
 
 int main(int argc, char *argv[])
 { 
-   pointerTest1(ages, names);
+   pointerTest1(*ages, names);
 
-    int count = sizeof(ages) / sizeof(int);
+    int count = sizeof(*ages) / sizeof(int);
     int i = 0;
     for(i = 0; i < count; i++){ 
         printf("%d in ages\n", ages[i]);
@@ -56,5 +64,8 @@ int main(int argc, char *argv[])
 
     pointerTest2(ages, names);
 
+    int sizeages = sizeof(ages);
+
+    printf("Size of ages: %d\n", sizeages);
     return 0;
 }
