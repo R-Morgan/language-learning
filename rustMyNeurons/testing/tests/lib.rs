8e48e206afa172c387d71ad82d87d46627fd4e3c
R-Morgan <rmorgan@trystero.is>
2014-10-22 16:17:26 -0700

    Add accessibility for add_three_times_four() for testing suite

--- rustMyNeurons/testing/tests/lib.rs
+++ rustMyNeurons/testing/tests/lib.rs
@@ -1,3 +1,6 @@
+extern crate testing;
+use testing::add_three_times_four;
+
 #[test]
 fn foo(){
     assert!(false);
@@ -8,3 +11,9 @@ fn bar(){
     assert!(true);
 }
 
+#[test]
+fn math_checks_out() {
+    let result = add_three_times_four(5i);
+
+    assert_eq!(32i, result);
+}
