ed8765fd266038379311cadd28aa6dde8fbc3699
R-Morgan <rmorgan@trystero.is>
2014-10-22 19:45:52 -0700

    Delete foo and bar test tests

--- rustMyNeurons/testing/tests/lib.rs
+++ rustMyNeurons/testing/tests/lib.rs
@@ -2,18 +2,10 @@ extern crate testing;
 use testing::add_three_times_four;
 
 #[test]
-fn foo(){
-    assert!(false);
-}
-
-#[test]
-fn bar(){
-    assert!(true);
-}
-
-#[test]
 fn math_checks_out() {
     let result = add_three_times_four(5i);
 
     assert_eq!(32i, result);
 }
+
+
