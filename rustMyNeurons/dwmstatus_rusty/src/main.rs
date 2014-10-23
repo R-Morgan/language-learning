6325ba981b535e679c11c22be77e1bcb0deb720d
R-Morgan <rmorgan@trystero.is>
2014-10-23 15:01:15 -0700

    Remove loop {} call from function body
    Removed the loop to execute within the body of main. Added a return
    statement to halt the program each time output is printed.
    
    It seems to be a matter of waiting long enough to observe the changes
    in load on the commandline. This function is working now, though!

--- rustMyNeurons/dwmstatus_rusty/src/main.rs
+++ rustMyNeurons/dwmstatus_rusty/src/main.rs
@@ -4,7 +4,9 @@ use std::io::BufferedReader;
 use std::io::File;
 
 fn main() {
-   get_loadavg();
+    loop { 
+       get_loadavg();
+    }
 }
 
 pub fn get_loadavg(){
@@ -14,33 +16,31 @@ pub fn get_loadavg(){
     // Path specification. What is up with Path::new, though. Why is Path
     // captialised?
 
-    loop { 
-        let mut file = BufferedReader::new(File::open(&path));
+    let mut file = BufferedReader::new(File::open(&path));
     // File's path seems to be referenced by a pointer to our path variable.
     // Is it that File::open() takes a pointer to a string? Or that this is a
     // &str? May very well be one and the same in Rust.
 
-        for line in file.lines() {
+    for line in file.lines() {
     // Starts for loop
-            let s: String = match line { 
-                Ok(s) => s,
-                Err(e)   => { 
+        let s: String = match line { 
+            Ok(s) => s,
+            Err(e)   => { 
             // The match syntax is useful for unwrapping results of type Result
-                    println!("No line!");
-                    return;
-                }
-            };
+                println!("No line!");
+                return;
+            }
+        };
         
         // By using the match syntax, it's possible to work with Result-types.
         // For Haskellers, this is similar to having to unwrapping Just a or
         // Maybe a
 
-            let sliced = s.as_slice();
-            let split:  Vec<&str> = sliced.split(' ').collect();
+        let sliced = s.as_slice();
+        let split:  Vec<&str> = sliced.split(' ').collect();
         // NB: the split() method requires single quotes in this instance
 
-            println!("{}", s);
-            println!("L: {}, {}, {}", split[0], split[1], split[2]);
-        }
+        println!("L: {}, {}, {}", split[0], split[1], split[2]);
+        return;
     }
 }
