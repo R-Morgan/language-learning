f452b753cf83477772b8afac3c648147d996d9a8
R-Morgan <rmorgan@trystero.is>
2014-10-23 11:06:24 -0700

    Add match syntax to work with Result of reading file
    In order to start accessing the elements of the contents of
    /proc/loadavg, it was necessary to employ match syntax so that
    it would be a String, that could be coerced to a &str.
    
    Programme now returns the cotents of /proc/loadavg at the
    moment of execution. A loop {} was added to main(); however,
    it just repeats the value at the time of execution, not the
    most recent.  Will need to figure out how to end the file
    read and do it again, I think.

--- rustMyNeurons/dwmstatus_rusty/src/main.rs
+++ rustMyNeurons/dwmstatus_rusty/src/main.rs
@@ -4,15 +4,41 @@ use std::io::BufferedReader;
 use std::io::File;
 
 fn main() {
-    get_loadavg();
+    loop { 
+        get_loadavg();
+    }
 }
 
 pub fn get_loadavg(){
+// Function declaration
 
     let path = Path::new("/proc/loadavg");
+    // Path specification. What is up with Path::new, though. Why is Path
+    // captialised?
     let mut file = BufferedReader::new(File::open(&path));
+    // File's path seems to be referenced by a pointer to our path variable.
+    // Is it that File::open() takes a pointer to a string? Or that this is a
+    // &str? May very well be one and the same in Rust.
+
     for line in file.lines() {
-        print!("{}", line.unwrap());
+    // Starts for loop
+        let s: String = match line { 
+            Ok(s) => s,
+            Err(e)   => { 
+            // The match syntax is useful for unwrapping results of type Result
+                println!("No line!");
+                return;
+            }
+        };
+        
+        // By using the match syntax, it's possible to work with Result-types.
+        // For Haskellers, this is similar to having to unwrapping Just a or
+        // Maybe a
+        let sliced = s.as_slice();
+        let split:  Vec<&str> = sliced.split(' ').collect();
+        // NB: the split() method requires single quotes in this instance
+        println!("{}", s);
+        println!("L: {}, {}, {}", split[0], split[1], split[2]);
     }
 }
 
